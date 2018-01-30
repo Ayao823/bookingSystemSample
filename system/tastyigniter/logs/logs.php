d2b482ad2836dfaf00131982882c9efecaa4862a
小笠原綾乃 <ayano@ayas-mbp.speedport_w_724v_typ_a_05011603_05_028>
2018-01-30 12:07:17 +0100

    put files in repo

---
+++ system/tastyigniter/logs/logs.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-01-29 16:52:06 --> cURL: Error --> 22: The requested URL returned error: 404 Not Found https://api.tastyigniter.com/v1/core/version/2.1.1/7.0.26/5.6.38
