21b46211de01ae7dc140a577920d0ca1625f4ef8
小笠原綾乃 <ayano@ayas-mbp.speedport_w_724v_typ_a_05011603_05_028>
2018-01-30 13:56:26 +0100

    Added error handling logic to index.php

--- index.php
+++ index.php
@@ -238,5 +238,33 @@ date_default_timezone_set('Europe/London');
  */
 require_once BASEPATH.'tastyigniter/core/TastyIgniter.php';
 
+define('ENVIRONMENT', 'development');
+/*
+ *---------------------------------------------------------------
+ * ERROR REPORTING
+ *---------------------------------------------------------------
+ *
+ * Different environments will require different levels of error reporting.
+ * By default development will show errors but testing and live will hide them.
+ */
+
+if (defined('ENVIRONMENT'))
+{
+    switch (ENVIRONMENT)
+    {
+        case 'development':
+            error_reporting(E_ALL);
+        break;
+
+    case 'testing':
+    case 'production':
+        error_reporting(0);
+    break;
+
+    default:
+        exit('The application environment is not set correctly.');
+}
+}
+
 /* End of file index.php */
 /* Location: ./index.php */
